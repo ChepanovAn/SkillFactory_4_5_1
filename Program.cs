429c3a33e1e767c1e217949b3ac70dbc7f3a860a
Анатолий <Chepanov199287@gmail.com>
2023-03-28 20:45:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory_4_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string name, string surname, char login, bool pitomec, int age, string[] color) User;
+        }
+    }
+}
